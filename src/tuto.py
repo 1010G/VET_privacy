e3a9f6da3f41c44d17b5e4c579570897ee5b40b1
Dylan <garnaud.dylan@gmail.com>
2019-02-11 18:14:09 +0100

    tuto.py modif pour changer le type 'y' de l'estimateur

--- src/tuto.py
+++ src/tuto.py
@@ -19,7 +19,7 @@ df2 = df2[df2['PurchasePrice']>= 500]
 df2 = df2[df2['DispositionSoldAmount']>= 500]
 df2 = df2[df2['DispositionSoldAmount'] <= df2['PurchasePrice']]
 
-df2['age']=df2['DispositionDate'] - df2['ModelYear']
+df2['Age']=df2['DispositionDate'] - df2['ModelYear']
 df2 = df2.drop(['DispositionDate','ModelYear'],axis=1)
 
 df2 = pd.get_dummies(df2,prefix=['FuelType','EngineConfiguration'])
@@ -36,16 +36,33 @@ LABELS = ["DispositionSoldAmount"] # y == predictions
 
 ## Convertions des types
 feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]			# creation des feature_collumn PAS ENCORE DE DATA juste les labels
+series=pd.Series(ydata['DispositionSoldAmount'].values) # creation d'un type "pd.series" pour le "y" de l'input estimateur
 
-print(feature_cols)
+def afficherDonnees(): #fonction temporaire pour afficher les data
+    print("\n   feature_cols")
+    print(feature_cols)
+    print("\n")
+    print("\nXdata")
+    print(Xdata)
+    print("\nydata")
+    print(ydata)
+    print("\nydataSeries")
+    print(series)
+    print("\n")
+
+afficherDonnees() # a commenter / decommenter pour afficher les data
+
 ## Création de l'estimateur
 estimator = tf.estimator.LinearRegressor(feature_columns=feature_cols,model_dir="train")
+
+## Ajout de la data dans l'estimateur
 def get_input_fn( num_epochs=None, n_batch = 128, shuffle=True):
          return tf.estimator.inputs.pandas_input_fn(
-         x= Xdata,
-         y = ydata,
-         batch_size=n_batch,
-         num_epochs=num_epochs,
-         shuffle=shuffle)
+            x=Xdata,                       # feature data                                               semble OK
+            y=series,                      # label data                                                 IDK
+            batch_size=n_batch,            # batch 128 by default                                       OK
+            num_epochs=num_epochs,         # number of epoch                                            OK
+            shuffle=shuffle)               # shuffle or not, yes by default                             OK
 
+## Execution du modéle
 estimator.train(input_fn=get_input_fn(num_epochs=None,n_batch = 128,shuffle=True),steps=1000)
