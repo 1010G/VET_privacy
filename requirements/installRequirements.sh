4fd19bf0e86d188860378283020388a0fec40df3
Dylan <garnaud.dylan@gmail.com>
2019-02-13 18:23:54 +0100

    requirements/installRequirements.sh modification du script pour garder la version du module de privacy utilisée durant le projet (lors d'une nouvelle installation on ne téléchargera pas la dernière version de ce module, mais on installera la vertion qui est sur le projet)

--- requirements/installRequirements.sh
+++ requirements/installRequirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install requirements for the VET_privacy project
 pip3 install -r ./requirementsPIP3.txt
-git clone https://github.com/tensorflow/privacy
+#git clone https://github.com/tensorflow/privacy 			# décommenter cette ligne pour télécharger la dernière version du module privacy
 pip3 install -e ./privacy/
